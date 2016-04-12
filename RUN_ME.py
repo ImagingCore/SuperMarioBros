ac8b3779707ecb79390cab7182ffa9150363ba7b
konstantin425 <kkopotiyenko@mgh.harvard.edu>
2016-04-12 13:46:52 -0400

    Added files via upload

---
+++ RUN_ME.py
@@ -0,0 +1,19 @@
+import GUI_Module_v2_3
+
+
+
+from Tkinter import *
+import tkFileDialog
+from os import path
+import csv
+
+
+
+def main():
+
+    GUI_Module_v2_3.main()
+
+if __name__ == "__main__":
+    main()
+
+
