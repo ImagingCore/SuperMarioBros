7a263984d67e85bb9ab7988a07441a6af61d0f2d
Linda Nieman <lnieman@mgh.harvard.edu>
2016-06-03 16:46:49 -0400

    Plotted all markers vs. time on page 1, added page 2 added ClinicalClassification annotation, stacked bar plot working with table.  (NOTE:  log2 of each marker, then summed) stacked bar changed to semi-log y-axis (NOTE:  no log2 transformation taken)

--- Plotting.py
+++ Plotting.py
@@ -25,22 +25,22 @@ def main(inputfile,outputdir):
     root, ext = os.path.splitext(filename)
 
     markers = pd.DataFrame(['FAT1-1', 'GRP143', 'IL13RA2', 'MAGEA2', 'MAGEC2', 'PMEL', 'SFRP1', 'TFAP2C', 'TNC', 'CSPG4', 'FAT2', 'GAGE1', 'MAGEA1', 'MAGEA4', 'MAGEA6', 'MLANA', 'PRAME', 'SOX10', 'TYRP1']);
-    markersTotals = pd.DataFrame([ 'TotalTranscriptsPerWellPerMlBlood', 'LineageSpecific', 'CarcinoEmbryonicAntigen', 'SignalTransduction']);
+    markersTotals = pd.DataFrame(['TotalTranscriptsPerWellPerMlBlood', 'LineageSpecific', 'CarcinoEmbryonicAntigen', 'SignalTransduction']);
     markers.rename(columns={0: 'Marker'}, inplace=True) # rename column
     markersTotals.rename(columns={0: 'MarkersTotals'}, inplace=True) # rename column
 
 
     # loop through each unique patient and plot time series
+    #patients = ['PEM01']
     for p in patients:
-
-        #p = 'PEM-15'
-        # pp = PdfPages('/Users/lindanieman/Documents/WORK/MGH CC/Droplets/MelData/PlotOutput/' + p + '_plots.pdf')
+        print 'patient= ' + p
+        #p = 'PEM01'
         pData = df.loc[df.Patient_ID == p]
 
         with PdfPages(outputdir + p + '_Log2.pdf') as pdf:
 
             fig = plt.figure(figsize=(12,8))
-            plt.suptitle(p + '\n Copies/mL vs. Time (weeks)', fontsize=14, fontweight='bold')
+            plt.suptitle(p + '\n Log2( Copies/mL + 1) vs. Time (weeks)', fontsize=14, fontweight='bold')
 
             # -------------------------------------------------
             # plot each marker separately as a function of time
@@ -55,7 +55,7 @@ def main(inputfile,outputdir):
                     plt.plot(x, y, '-ro', markersize=10)
 
                     #plt.ylabel(markers.ix[m][0] + '\n (copies / mL)', fontsize = 8)
-                    plt.ylabel('log2(' + markers.ix[m][0] + '\n (copies / mL)', fontsize=8) # log2(x+1)
+                    plt.ylabel(markers.ix[m][0] + '\n (copies / mL)', fontsize=8) # log2(x+1)
 
                     plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=0.5, hspace=0.4)
                     ax = plt.gca()
@@ -67,96 +67,104 @@ def main(inputfile,outputdir):
                 else:
                     print 'Number of markers is greater than 20! \Modify for loop code'
 
-        pdf.savefig()
-        plt.close()
-        #plt.show()
+            pdf.savefig()
+            plt.close()
+            #plt.show()
 
-        # ---------------------------------------------------------------------
-        # plot totals, lineaage specific, carcinoembryonic, signaltransduction
-        fig = plt.figure()
-        plt.suptitle(p + '\n Copies/mL vs. Time (weeks)', fontsize=14, fontweight='bold')
-        for n in xrange(0,len(markersTotals)):
+            # ---------------------------------------------------------------------
+            # plot totals, lineaage specific, carcinoembryonic, signaltransduction
+            fig = plt.figure()
+            plt.suptitle(p + '\n Copies/mL vs. Time (weeks)', fontsize=14, fontweight='bold')
+            for n in xrange(0,len(markersTotals)):
 
-            plt.subplot(2,2,n+1)
-            x = pData.TimeFromInitialBloodDraw_weeks
-            #y = pData[markersTotals.ix[n][0]]
-            y = np.log2(pData[markersTotals.ix[n][0]] +1) # take log2(x+1)
-            plt.plot(x, y, '-ro')
-            plt.title(markersTotals.ix[n][0], fontsize=12)
-            plt.ylabel('log2(x+1)' + '\n (copies / mL)', fontsize=12)
+                plt.subplot(2,2,n+1)
+                x = pData.TimeFromInitialBloodDraw_weeks
+                #y = pData[markersTotals.ix[n][0]]
+                y = np.log2(pData[markersTotals.ix[n][0]] +1) # take log2(x+1)
+                plt.plot(x, y, '-ro')
+                plt.title(markersTotals.ix[n][0], fontsize=12)
+                plt.ylabel('log2(x+1)' + '\n (copies / mL)', fontsize=12)
 
-            plt.subplots_adjust(left=0.15, bottom=None, right=0.95, top=0.85, wspace=0.5, hspace=0.4)
-            ax = plt.gca()
-            ax.tick_params(axis='y', labelsize=8)
-            ax.tick_params(axis='x', labelsize=8)
+                plt.subplots_adjust(left=0.15, bottom=None, right=0.95, top=0.85, wspace=0.5, hspace=0.4)
+                ax = plt.gca()
+                ax.tick_params(axis='y', labelsize=8)
+                ax.tick_params(axis='x', labelsize=8)
 
-            ## add ClinicalClassification annotation
-            ccLabels = pData.ClinicalClassification.reset_index().ix[:,1].tolist() #label list for given patient
+                ## add ClinicalClassification annotation
+                ccLabels = pData.ClinicalClassification.reset_index().ix[:,1].tolist() #label list for given patient
 
-            # for label, X, Y in zip(ccLabels, x, y):
-            #     plt.annotate(label, xy=(X,Y), xytext=(-20,20), textcoords='offset points',
-            #                  ha='left', va='bottom', bbox = dict(boxstyle = 'round,pad=0.25', fc = 'yellow', alpha = 0.50),
-            #                  arrowprops = dict(arrowstyle = '->', connectionstyle = 'arc3,rad=0'),fontsize=8)
+                # for label, X, Y in zip(ccLabels, x, y):
+                #     plt.annotate(label, xy=(X,Y), xytext=(-20,20), textcoords='offset points',
+                #                  ha='left', va='bottom', bbox = dict(boxstyle = 'round,pad=0.25', fc = 'yellow', alpha = 0.50),
+                #                  arrowprops = dict(arrowstyle = '->', connectionstyle = 'arc3,rad=0'),fontsize=8)
 
             #plt.show()
-        pdf.savefig()
-        plt.close()
+            pdf.savefig()
+            plt.close()
 
-    #p = 'PEM-15'
-    #pData = df.loc[df.Patient_ID == p]
+        #p = 'PEM-15'
+        #pData = df.loc[df.Patient_ID == p]
 
-    def stackedBarPlots(pData, markers):
-        # this function plots a stacked bar graph
+            def stackedBarPlots(pData, markers):
+                # this function plots a stacked bar graph
 
-        markers.sort_values('Marker', axis=0, inplace=True)  # sort markers names alphabetically
-        markers.reset_index(drop=True, inplace=True)
+                markers.sort_values('Marker', axis=0, inplace=True)  # sort markers names alphabetically
+                markers.reset_index(drop=True, inplace=True)
 
-        fig_bar = plt.figure(figsize=(10, 8))
-        plt.subplot(211)
+                fig_bar = plt.figure(figsize=(10, 8))
+                plt.subplot(211)
 
-        bar_x = np.arange(len(pData.TimeFromInitialBloodDraw_weeks)) + 0.2
-        barWidth = 0.4
-        y_offset = np.array([0.0] * len(bar_x))
+                bar_x = np.arange(len(pData.TimeFromInitialBloodDraw_weeks)) + 0.2
+                barWidth = 0.75
+                y_offset = np.array([0.0] * len(bar_x))
 
-        # Get colors for bars
-        colors = plt.cm.Set1(np.linspace(0, 0.75, len(markers)))
+                # Get colors for bars
+                colors = plt.cm.Set1(np.linspace(0, 0.75, len(markers)))
 
-        cell_text2 = []
-        for m in xrange(0, len(markers)):  # (start,stop,step)
+                #cell_text2 = []
+                for m in xrange(0, len(markers)):  # (start,stop,step)
+                    #print('marker = ' + marker.ix[m])
 
-            if len(markers) <= 20:
-                y = np.log2(pData[markers.ix[m][0]] + 1)  # take log2(x+1)
-                plt.bar(bar_x, y, barWidth, bottom=y_offset, color=colors[m], label=markers.ix[m, 'Marker'])
-                y_offset = y_offset + y
+                    if len(markers) <= 20:
+                        y = pData[markers.ix[m][0]]  # np.log2(pData[markers.ix[m][0]] + 1)  # take log2(x+1)
+                        plt.bar(bar_x, y, barWidth, bottom=y_offset, color=colors[m], label=markers.ix[m, 'Marker'])
+                        y_offset = y_offset + y
 
-            else:
-                print 'Number of markers is greater than 20! \Modify for loop code'
+                    else:
+                        print 'Number of markers is greater than 20! \Modify for loop code'
 
-        plt.ylabel('log2(x+1)' + '\n (copies / mL)', fontsize=16)
-        plt.xlabel('Time (weeks)', fontsize=16)
-        plt.title(p, fontsize=18)
-        plt.xticks(pd.Series(bar_x) + barWidth / 2, pData.TimeFromInitialBloodDraw_weeks)
-        plt.subplots_adjust(left=None, bottom=None, right=0.75, top=None, wspace=None, hspace=None)
-        plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0,
-                   fontsize=10)  # colors,markersSorted['Marker'])
+                #plt.ylabel('log2(x+1)' + '\n (copies / mL)', fontsize=16)
+                plt.ylabel('Copies / mL', fontsize=16)
+                plt.xlabel('Time (weeks)', fontsize=16)
+                plt.title(p, fontsize=18)
+                plt.xticks(pd.Series(bar_x) + barWidth / 2, pData.TimeFromInitialBloodDraw_weeks)
+                plt.subplots_adjust(left=0.15, bottom=None, right=0.82, top=None, wspace=None, hspace=None)
+                plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0,
+                           fontsize=10)  # colors,markersSorted['Marker'])
+                ax1 = plt.gca()
+                ax1.set_yscale('log',nonposy='clip')
 
-        plt.subplot(212)
-        ax = plt.gca()
-        ax.axis('off')
-        cellTEXT = [[txt] for txt in pData.ClinicalAnnotations]
-        rowLABELS = [(str(rtxt) + ' weeks') for rtxt in pData.TimeFromInitialBloodDraw_weeks.tolist()]
-        colLABELS = ['Clinical Annotation']
-        the_table = plt.table(cellText=cellTEXT, colLabels=colLABELS, rowLabels=rowLABELS, loc='center',
-                              cellLoc='center')
-        # or the_table = ax.table(...)
-        the_table.set_fontsize(14)
-        the_table.scale(1, 1.5)
-        table_props = the_table.properties()
-        table_cells = table_props['child_artists']
-    #stackedBarPlots(pData,markers)
 
-        pdf.savefig()
-        plt.close()
+                plt.subplot(212)
+                ax = plt.gca()
+                ax.axis('off')
+                cellTEXT = [[txt] for txt in pData.ClinicalAnnotations]
+                rowLABELS = [(str(rtxt) + ' weeks') for rtxt in pData.TimeFromInitialBloodDraw_weeks.tolist()]
+                colLABELS = ['Clinical Annotation']
+                the_table = plt.table(cellText=cellTEXT, colLabels=colLABELS, rowLabels=rowLABELS, loc='center',
+                                      cellLoc='center')
+                # or the_table = ax.table(...)
+                the_table.set_fontsize(12)
+                the_table.scale(1, 2)
+                table_props = the_table.properties()
+                table_cells = table_props['child_artists']
+
+
+            stackedBarPlots(pData,markers)
+
+            #plt.show()
+            pdf.savefig()
+            plt.close()
 
     if outputdir == None:
         foo = None
@@ -172,18 +180,8 @@ def main(inputfile,outputdir):
         #     return statusOut, statusColor, statusDoneOut, statusDoneColor
 
 
-
-    #stackedBarPlots(pData,markers)
-
-
-    # add clinical annotations as table in subplot
-
     #plt.close('all')
 
 #
 if __name__ == "__main__":
     main(inputfile, outputdir)
-
-
-
-    ### TEST
